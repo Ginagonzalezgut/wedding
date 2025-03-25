182573b9b0c820b39680e5fbc9df25a3f75f6d2b
Gina González <gini_go@hotmail.com>
2025-03-25 15:57:46 +0100

    add lightbox to galery

--- wp-content/themes/wedding/functions.php
+++ wp-content/themes/wedding/functions.php
@@ -26,11 +26,12 @@ function wedding_styles()
     wp_enqueue_style('owl.carousel-css', esc_url(get_template_directory_uri()) . "/css/owl.carousel.min.css", array(), "2.3.4");
     wp_enqueue_style('owl.theme.default-css', esc_url(get_template_directory_uri()) . "/css/owl.theme.default.min.css", array("owl.carousel-css"), "2.3.4");
     wp_enqueue_style("reset", "https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css", array(), "1.0.0");
+    wp_enqueue_style('lightboxcss', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.5');
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), "6.5.1");
-    wp_enqueue_style("style", get_stylesheet_uri(), array("reset", "font-awesome"), "1.0.0");
+    wp_enqueue_style('style', get_stylesheet_uri(), array("reset", "font-awesome"), "1.0.0");
 
     // JavaScript
-    wp_enqueue_script('jquery', esc_url(get_template_directory_uri()) . '/js/jquery-3.7.1.min.js', array(), "3.7.1");
-    wp_enqueue_script('owl.carousel-js', esc_url(get_template_directory_uri()) . '/js/owl.carousel.min.js', array('jquery'), "2.3.4");
+    wp_enqueue_script('lightboxjs', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), "2.11.5", true);
+    wp_enqueue_script('owl.carousel-js', esc_url(get_template_directory_uri()) . '/js/owl.carousel.min.js', array('jquery'), "2.3.4", true);
 }
 add_action("wp_enqueue_scripts", "wedding_styles");
--- wp-content/themes/wedding/page-galeria.php
+++ wp-content/themes/wedding/page-galeria.php
@@ -31,7 +31,7 @@ get_header();
                 $imagen_full = wp_get_attachment_image_src($id, "full")[0];
             ?>
                 <li>
-                    <a href="<?php echo $imagen_full; ?>">
+                    <a data-lightbox="galeria" href="<?php echo $imagen_full; ?>">
                         <img src="<?php echo $imagen_grande; ?>" alt="imagengaleria" />
                     </a>
                 </li>
